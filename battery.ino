0c1efd6e3a97f3358f06ad2dcbcc50662613bae6
yasastharinda9511 <37435024+yasastharinda9511@users.noreply.github.com>
2019-03-25 22:29:26 +0530

    Add files via upload

---
+++ battery.ino
@@ -0,0 +1,117 @@
+void battery(){
+  v1=((float(analogRead(res1))/1024)*5)*6.9/3.6;
+  v2=((float(analogRead(res2))/1024)*5)*6.9/3.6;
+  v3=((float(analogRead(res3))/1024)*5)*4.6/2.6;
+  float percentage=(v2-v3)*100/9;
+  if(charged and!(v1-v2<0 and (v1-v2)/10<-0.025)){
+    lcd.clear();
+    Serial.println("charged");
+    lcd.setCursor(0,0);
+    lcd.print("....Charged....");
+    delay(1000);
+    return;
+    }
+  
+  if(discharging and percentage<10){
+    analogWrite(mosfet_base,0);
+    lcd.clear();
+    Serial.println("Caution");
+    lcd.setCursor(0,1);
+    lcd.print("Caution");
+    return ;
+    }
+  if(percentage>=95 and (v1-v2)/10>0.025 ){
+    analogWrite(mosfet_base,0);
+    Serial.println("charged");
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("....Charged....");
+    delay(1000);
+    charged=true;
+    return;
+    }
+  if (v1-v2<0 and (v1-v2)/10<-0.025 ){
+    analogWrite(mosfet_base,255);
+    Serial.println("discharging to the load");
+    Serial.print("battery percentage:");
+    Serial.print((v2-v3)*100/9);
+    Serial.println("%");
+    Serial.print("current through the battery:");
+    Serial.println((v1-v2)/10);
+    discharging=true;
+    charged=false;
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Discharging");
+    for(int i=0;i<count;i++){
+      lcd.print(".");
+      }
+    if(count==6){
+      count=0;
+      }else{
+        count=count+1;
+        }
+     lcd.setCursor(0,1);
+     lcd.print("Battery : ");
+     lcd.print(int(((v2-v3)*100/9)+8));
+     lcd.print(" %");
+     delay(1000);
+    return;
+    }
+  if(v1-v2<0){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("....No Load....");
+    delay(1000);
+    return;
+    }
+  if(v2-v3<=9 && v1!=0 && v2!=0 && v3!=0){
+    analogWrite(mosfet_base,255);
+    Serial.println("charging");
+    Serial.print("battery voltage:");
+    Serial.println(v2-v3);
+    Serial.print("current through the battery:");
+    Serial.println((v1-v2)/10);
+    Serial.print("battery percentage:");
+    Serial.print((v2-v3)*100/9);
+    Serial.println("%");
+    
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Charging");
+    for(int i=0;i<count1;i++){
+      lcd.print(".");
+      }
+    if(count1==9){
+      count1=0;
+      }else{
+        count1=count1+1;
+        }
+    /*lcd.setCursor(0,1);
+    lcd.print("Vol:");
+    lcd.print(v2-v3);
+    lcd.print("V");
+
+    lcd.print("I:");
+    lcd.print((v1-v2)/10);*/
+    lcd.setCursor(0,1);
+    lcd.print("Battery : ");
+    lcd.print(int((v2-v3)*100/9));
+    lcd.print(" %");
+    delay(1000);
+    discharging=false;
+    return;
+    }
+  if(v3==0){
+    Serial.println("battery is not connected");
+    lcd.clear();
+    discharging=false;
+    charged=false;
+    lcd.setCursor(0,0);
+    lcd.print("     Battery      ");
+    lcd.setCursor(0,1);
+    lcd.print("Is Not Connected");
+    delay(2000);
+    return ;
+    }
+  }
---
+++ project_code.ino
@@ -0,0 +1,38 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+#define mosfet_base 9
+#define res1 A0
+#define res2 A1
+#define res3 A2
+
+float v1;
+float v2;
+float v3;
+boolean discharging =false;
+int count=0;
+int count1=0;
+boolean charged=false;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+void setup() {
+  Serial.begin(9600);
+  pinMode(mosfet_base,OUTPUT);
+  pinMode(res1,INPUT);
+  pinMode(res2,INPUT);
+  pinMode(res3,INPUT);
+  lcd.begin();
+  lcd.setCursor(0,0);
+  lcd.print(" BATTERY CHARGER ");
+  lcd.setCursor(0,1);
+  lcd.print(" AND DISCHARGER! ");
+  delay(3000);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  battery();
+  //delay(100);
+  Serial.println("##############");
+}
